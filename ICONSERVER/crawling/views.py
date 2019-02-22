37c10b2d0f5119ed0a1146f43e75bda40d5ad516
JeongLeeyop <anfdmavy777@naver.com>
2019-02-22 18:10:28 +0900

    1

--- ICONSERVER/crawling/views.py
+++
@@ -1,7 +0,0 @@
-from .models import Post
-from django.shortcuts import render
-
-def index(request):
-    a = 20190217
-    posts = list(Post.objects.filter(date=a))
-    return render(request, 'crawling/index.html', {'posts' : posts})
